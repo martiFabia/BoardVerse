d9dba43c5ffa00c4d405c72f2aa66215b69a9f9b
Martina <fabiani.martina@icloud.com>
2025-01-12 20:46:35 +0100

    Create Spring Boot project

---
+++ BoardVerse/src/main/java/com/example/BoardVerse/BoardVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoardVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardVerseApplication.class, args);
+	}
+
+}
